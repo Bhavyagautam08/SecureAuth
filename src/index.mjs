82ac1a803dc4da94381720401578aa658daec41e
Bhavyagautam08 <bhavyagautam0608@gmail.com>
2026-01-18 16:29:01 +0530

    Update SecureAuth code

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+// --- NEW IMPORTS ---
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+// -------------------
+
 dotenv.config(); // MUST be first
 import connectDB from "./config/db.mjs";
 import authRoutes from "./routes/auth.routes.mjs";
@@ -7,7 +12,19 @@ import authRoutes from "./routes/auth.routes.mjs";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true, 
+  legacyHeaders: false, 
+});
+
+app.use(limiter);
+
 app.use(express.json());
+
+app.use(mongoSanitize());
 
 connectDB(); 
 
